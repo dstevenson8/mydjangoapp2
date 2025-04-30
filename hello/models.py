8ebdca0a6b154a0a55d7be52d511212e261a8d33
David Stevenson <dstevenson@egnyte.com>
2025-04-30 14:35:25 -0500

    added name list

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class NameEntry(models.Model):
+    name = models.CharField(max_length=100)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- hello/views.py
+++ hello/views.py
@@ -6,4 +6,9 @@ def hello_view(request):
     name = None
     if request.method == "POST":
         name = request.POST.get("name")
+        if name:
+            NameEntry.objects.create(name=name)
+            return redirect('/')
+            
+    names = NameEntry.objects.order_by('-submitted_at')
     return render(request, "hello.html", {"name": name})
